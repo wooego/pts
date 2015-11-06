882c2b66ba5a7d1ae3aa7bf7e4f2ae7e33d9aeef
fp862 <fp862@163.com>
2015-11-06 11:26:18 +0800

    对用户的专业和类型进行限定，仅能从指定的列表中选择，方便与题库的数据字段匹配

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,36 @@
+# coding:utf-8
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
+    JUNXIE = 'JX'
+    HANGDIAN = 'HD'
+    JIXIE = 'XE'
+    SPECIALTY_CHOICES = (
+        (JUNXIE, '军械'),
+        (HANGDIAN, '航电'),
+        (JIXIE, '机械'),
+    )
+
+    FDZ = 'FDZ'
+    SHI = 'SHI'
+    YUAN = 'YUAN'
+    POSITION_CHOICES = (
+        (FDZ, '分队长以上'),
+        (SHI, '师'),
+        (YUAN, '员'),
+    )
     user = models.OneToOneField(User, related_name='profile')
-    specialty = models.CharField(max_length=10)
-    position = models.CharField(max_length=5)
+    specialty = models.CharField(max_length=2,
+                                 choices=SPECIALTY_CHOICES, )
+    position = models.CharField(max_length=4,
+                                choices=POSITION_CHOICES, )
+
+    def __unicode__(self):
+        return self.user.username + " : " + self.specialty + " : " + self.position
 
 
 def create_user_profile(sender, instance, created, **kwargs):

fc15f3f7e2f521efe48c259caab1eef54abf027d
fp862 <fp862@163.com>
2015-11-11 21:33:48 +0800

    新建PaperPartsPercent类，以记录试卷比例，但是不知道如何限制只存储一组数据

---
+++ exam/models.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+from django.core.exceptions import ValidationError
+
+from django.db import models
+
+
+class PaperPartsPercent(models.Model):
+    domain = models.IntegerField(verbose_name='专业基础题比例', )
+    regulation = models.IntegerField(verbose_name='法规题比例', )
+    hightech = models.IntegerField(verbose_name='高科技题比例', )
+    domainprimary = models.IntegerField(verbose_name='专业题比例', )
+
+
+    def __str__(self):
+        return str(self.regulation) + ":" + str(self.hightech) + ":" + str(self.domain) + ":" + str(self.domainprimary)
+
+    def __unicode__(self):
+        return str(self.regulation) + ":" + str(self.hightech) + ":" + str(self.domain) + ":" + str(self.domainprimary)
+
+    def clean(self):
+        if self.regulation + self.hightech + self.domain + self.domainprimary != 100:
+            raise ValidationError('题目比例不正确，请保证其和为100')
+
+
+# Create your models here.

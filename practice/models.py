e9d37e44a817746599935e80f536fd6e79688c05
fp862 <fp862@163.com>
2015-11-14 16:41:46 +0800

    去除高科技类型，增加所有人员和所有专业两个选项

--- practice/models.py
+++ practice/models.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -10,14 +10,13 @@ class RandomManager(models.Manager):
         return super(RandomManager, self).get_queryset().order_by('?')
 '''
 
+
 class Question(models.Model):
     Regulations = 'RL'
-    HighTech = 'HT'
     Domain = 'DM'
     DomainPrimary = 'DP'
     TYPE_CHOICES = (
         (Regulations, '法规'),
-        (HighTech, '高科技'),
         (Domain, '专业'),
         (DomainPrimary, '专业基础')
     )
@@ -25,7 +24,9 @@ class Question(models.Model):
     JUNXIE = 'JX'
     HANGDIAN = 'HD'
     JIXIE = 'XE'
+    ALL = 'AL'
     SPECIALTY_CHOICES = (
+        (ALL, '所有专业'),
         (JUNXIE, '军械'),
         (HANGDIAN, '航电'),
         (JIXIE, '机械'),
@@ -34,13 +35,15 @@ class Question(models.Model):
     FDZ = 'FDZ'
     SHI = 'SHI'
     YUAN = 'YUAN'
+    ALL = 'ALL'
     POSITION_CHOICES = (
+        (ALL, '所有人员'),
         (FDZ, '分队长以上'),
         (SHI, '师'),
         (YUAN, '员'),
     )
 
-    content = models.CharField(max_length=300,verbose_name=u'题目内容')  # 题目内容
+    content = models.CharField(max_length=300, verbose_name=u'题目内容')  # 题目内容
     rightOption = models.CharField(max_length=4)  # 最多有四个正确选项
     specialty = models.CharField(max_length=2,
                                  choices=SPECIALTY_CHOICES, )
@@ -58,19 +61,21 @@ class Question(models.Model):
     def __unicode__(self):
         return self.content
 
+
 class Answer(models.Model):
     question = models.ForeignKey(Question)
     option = models.CharField(max_length=1)  # 答案选项，如ABCD
     content = models.CharField(max_length=200)  # 答案的内容
 
     def __str__(self):
-        return self.option+":"+self.content
+        return self.option + ":" + self.content
 
     def __unicode__(self):
-        return self.option+":"+self.content
+        return self.option + ":" + self.content
 
     class Meta:
-        ordering = ['question','option']
+        ordering = ['question', 'option']
+
 
 class MasterStatus(models.Model):
     user = models.ForeignKey(User)
@@ -78,8 +83,9 @@ class MasterStatus(models.Model):
     is_master = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.user.username+":"+self.question.content+":"+str(self.is_master)
+        return self.user.username + ":" + self.question.content + ":" + str(self.is_master)
 
     def __unicode__(self):
-        return self.user.username+":"+self.question.content+":"+str(self.is_master)
+        return self.user.username + ":" + self.question.content + ":" + str(self.is_master)
+
 # Create your models here.

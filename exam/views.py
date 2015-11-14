32ad58123940e38451885f0c1f0928b5805a82d0
fp862 <fp862@163.com>
2015-11-14 23:37:05 +0800

    新建result

--- exam/views.py
+++ exam/views.py
@@ -11,12 +11,25 @@ def exam(request):
     percents = PaperPartsPercent.objects.all()[0] #
     domains = Question.objects.filter(specialty=specialty, position=position, type=Question.Domain).order_by('?').all()#[percents.domain]
     regulations = Question.objects.filter(specialty=specialty, position=position, type=Question.Regulations).order_by('?').all()#[percents.regulation]
-    hightechs= Question.objects.filter(specialty=specialty, position=position, type=Question.HighTech).order_by('?').all()#[percents.hightech]
     domainprimarys= Question.objects.filter(specialty=specialty, position=position, type=Question.DomainPrimary).order_by('?').all()#[percents.domainprimary]
 
-    questions = domains | regulations | hightechs | domainprimarys
+    questions = domains | regulations | domainprimarys
     answers_list = []
     for question in questions:
         answers = Answer.objects.filter(question_id=question.id)  # .order_by('option')
         answers_list.append(answers)
     return render(request, 'exam/exam.html', {"questions": questions,"answers_list": answers_list})
+
+
+def result(request):
+    #return HttpResponse(request.POST.items())
+    wrongs = 0
+    post_data = request.POST
+    for item in post_data.items():
+        #print item[0],type(item[0]),'--->',item[1],type(item[1])
+        if item[0].startswith('ro'):
+            opt = item[0][2:]
+            if post_data.get(opt) != post_data.get(item[0]):
+                wrongs += 1
+    ret = '共错了 '+str(wrongs)+' 道题\n'
+    return HttpResponse(ret)

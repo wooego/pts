a39305c1a1f18dc3c95e816d15f25b3398c39d2e
fp862 <fp862@163.com>
2015-11-30 18:36:57 +0800

    增加选择题目类型的功能

--- practice/forms.py
+++ practice/forms.py
@@ -1,11 +1,25 @@
 # coding:utf-8
 from django import forms
+from django.db import models
 # from django.contrib.auth.models import User
 
 class ConfigureForm(forms.Form):
+    ALL = 'AL'
+    Regulations = 'RL'
+    Domain = 'DM'
+    DomainPrimary = 'DP'
+
+    TYPE_CHOICES = (
+        (ALL, '全部'),
+        (Regulations, '法规'),
+        (Domain, '专业'),
+        (DomainPrimary, '专业基础')
+    )
+
     each_page = forms.IntegerField(
         required=True,
         min_value=1,
+        max_value=100,
         label=u"每页显示的题目数量",
         error_messages={'required': u'请输入每页显示的题目数量'},
         widget=forms.NumberInput(
@@ -14,6 +28,10 @@ class ConfigureForm(forms.Form):
             }
         ),
     )
+    question_type = forms.ChoiceField(
+                                    choices = TYPE_CHOICES,
+                                    label = "题目类型：",
+                                    )
     dmp = forms.BooleanField(
         label=u"显示已经掌握的题目",
         initial=True,

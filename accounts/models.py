515142da16401bc15b614f8ba0f74092c46740c7
fp862 <fp862@163.com>
2015-11-30 18:27:42 +0800

    将航电换为特设和无线电

--- accounts/models.py
+++ accounts/models.py
@@ -6,18 +6,22 @@ from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
-    JUNXIE = 'JX'
-    HANGDIAN = 'HD'
     JIXIE = 'XE'
+    JUNXIE = 'JX'
+    TESHE = 'TS'
+    WUXIANDIAN = 'WX'
+
     SPECIALTY_CHOICES = (
-        (JUNXIE, '军械'),
-        (HANGDIAN, '航电'),
         (JIXIE, '机械'),
+        (JUNXIE, '军械'),
+        (TESHE,'特设'),
+        (WUXIANDIAN,'无线电'),
     )
 
     FDZ = 'FDZ'
     SHI = 'SHI'
     YUAN = 'YUAN'
+    
     POSITION_CHOICES = (
         (FDZ, '分队长以上'),
         (SHI, '师'),
@@ -32,13 +36,11 @@ class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.username + " : " + self.specialty + " : " + self.position
 
-
+'''
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         profile, created = UserProfile.objects.get_or_create(user=instance)
 
 
 post_save.connect(create_user_profile, sender=User)
-
-
-
+'''
